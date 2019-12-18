b9a77efc36dd44d5b24ad65c800e65f82946aad2
Chibashini97 <chibashini2014@gmail.com>
2019-12-18 18:33:55 +0530

    2 files modified, 1 added

--- components/Register.js
+++ components/Register.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './styles.css';
 
-function Register (){
+class Register extends Component{
+
+  constructor(){
+    super();
+    this.state ={
+      selectedOption:'EAN'
+    }
+
+    this.handleOptionChange = this.handleOptionChange.bind(this);
+  }
+
+  handleOptionChange(e) {
+  this.setState({
+    selectedOption: e.target.value
+  });
+  console.log(this.state.selectedOption);
+  }
+
+  render(){
   return(
-    <div><div className = "heading">Customer Pricing Map</div><br/>
+    <div>
+      <div className = "heading">Customer Pricing Map</div><br/>
       <form>
-      <div className="first"><div className = "heading">Level ID/ EAN</div><br/>
-        <input type="radio" name="group1" value="Level" checked />  Level ID: <input className = "second" type="text" / >
-        <br/>
-        <input type="radio" name="group1" value="EAN" /> EAN
-      </div>
-      <div className="first"><div className = "heading">Pricing Loaded Option</div><br/>
-        <input type="radio" name="group2" value="price" checked />  Pricing Loaded only <br/>
-        <input type="radio" name="group2" value="price1" /> Pricing Loaded with
-      </div>
-      <div className="first"><div className = "heading">Hierarchy Comparison</div><br/>
-        <input type="radio" name="group3" value="price2" checked />  Compare Down<br/>
-        <input type="radio" name="group3" value="price3" /> Compare across
-      </div>
-     <div className="first"><div className = "heading">Excluded Status</div><br/>
-        <input type="checkbox" name="check" value="xxx"/>Cash<br/>
-        <input type="checkbox" name="check" value="yyy"/> Delete
-      </div>
-      <button className="buttonAlign">Search </button>
+        <div className="first"><div className = "heading">Level ID/ EAN</div><br/>
+          <input type="radio" name="group1" value="Level" checked={this.state.selectedOption === 'EAN'} onChange={this.handleOptionChange} />  Level ID: <input className = "second" type="text" / >
+          <br/>
+          <input type="radio" name="group1" value="EAN" checked={this.state.selectedOption === 'Level'} onChange={this.handleOptionChange} /> EAN
+        </div>
+        <div className="first"><div className = "heading">Pricing Loaded Option</div><br/>
+          <input type="radio" name="group2" value="price"  />  Pricing Loaded only <br/>
+          <input type="radio" name="group2" value="price1" /> Pricing Loaded with
+        </div>
+        <div className="first"><div className = "heading">Hierarchy Comparison</div><br/>
+          <input type="radio" name="group3" value="price2"  />  Compare Down<br/>
+          <input type="radio" name="group3" value="price3" /> Compare across
+        </div>
+        <div className="first"><div className = "heading">Excluded Status</div><br/>
+          <input type="checkbox" name="check" value="xxx"/>Cash<br/>
+          <input type="checkbox" name="check" value="yyy"/> Delete
+        </div>
+        <button className="buttonAlign">Search </button>
       </form>
     </div>
   )
+}
 }
 
 export default Register
